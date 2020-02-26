efa49a343d42c31ca6209654ae7bf0190a8d916c
Cian McElhinney <cianmce@gmail.com>
2020-02-26 22:41:49 +0000

    add index html

--- slow_lane.rb
+++ slow_lane.rb
@@ -15,14 +15,14 @@ get "/stream*" do
   content = params[:ct] || params[:content_type] || "text/plain"
   content_type content
 
-  cycles = params[:c] || params[:cycles] || 10
+  cycles = params[:c] || params[:count] || 10
   cycles = cycles.to_i
   delay = params[:d] || params[:delay] || 0.4
   delay = delay.to_f
 
   stream do |out|
     cycles.times do
-      text = params[:text] || SecureRandom.hex(32)
+      text = params[:t] || params[:text] || SecureRandom.hex(32)
       out << "#{text}\n"
       puts "sleeping for #{delay}"
       sleep delay if delay > 0
